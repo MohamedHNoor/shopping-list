4a6fa1d7fab215fd79d30baa0f1b02df43f5c32a
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2025-04-30 17:27:51 +1200

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('item-form')
+const input = document.getElementById('item-input')
+const list = document.getElementById('item-list')
+
+// add new Item function
+function addItem(e) {
+  e.preventDefault()
+
+  const newItem = input.value
+
+  // validate input
+  if (newItem === '') {
+    alert('Please add an item!')
+    return
+  }
+
+  // create an item
+  const li = document.createElement('li')
+  li.appendChild(document.createTextNode(newItem))
+  const button = createButton('remove-item btn-link text-red')
+
+  li.appendChild(button)
+  list.appendChild(li)
+
+  input.value = ''
+}
+
+function createButton(classes) {
+  const button = document.createElement('button')
+  button.className = classes
+  const icon = createIcon('fa-solid fa-xmark')
+  button.appendChild(icon)
+  return button
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i')
+  icon.className = classes
+  return icon
+}
+
+// event listener
+form.addEventListener('submit', addItem)
